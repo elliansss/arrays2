06aa87c7253ff70b069056e225e47182ecbd073e
elliansss <rockismyl1fe@yandex.ru>
2025-03-29 20:02:13 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,60 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static int[] generateRandomArray() {
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100_000);
+        }
+        return array;
+    }
+
+    public static void task1() {
+        System.out.println("task 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        System.out.println("Сумма завтрат за месяц составила " + sum + " рублей");
+        //task 2
+
+        System.out.println("task 2");
+        int[] weeklyCost = {1000, 2500, 3300, 2370, 4500};
+        int minAmount = weeklyCost[0];
+        int maxAmount = weeklyCost[0];
+        for (int i = 1; i < weeklyCost.length; i++) {
+            if (weeklyCost[i] < minAmount) {
+                minAmount = weeklyCost[i];
+
+            }
+            if (weeklyCost[i] > maxAmount) {
+                maxAmount = weeklyCost[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + minAmount + " рублей.");
+        System.out.println("Максимальная сумма трат за неделю составила " + maxAmount + " рублей.");
+
+        //task 3
+        System.out.println("task 3");
+        int[] monthlyCost = {7800, 5600, 6430, 8300, 4980};
+        int sum2 = 0;
+        for (int i = 1; i < monthlyCost.length; i++) ;
+        int i = 1;
+        sum2 += monthlyCost[i];
+        sum2 = sum2 / 5;
+        System.out.println("Средняя сумма трат за месяц составила " + sum2 + " рублей.");
+        //task 4
+        System.out.println("task 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
+
+
+
